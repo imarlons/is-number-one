8af5209d95928e78c2c8f7d5c5d0ee1f65dc703b
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-03-14 08:22:15 -0300

    script inicial

---
+++ script-pro.py
@@ -0,0 +1,8 @@
+# entrada
+numero = int(input('Informe um número: '))
+
+# processamento & saída
+if (numero == 1):
+    print('O seu número é o NÚMERO 1')
+else:
+    print('O seu número não é o NÚMERO 1')
